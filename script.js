062268a290e7182a4d5426e5de7ddbc81bd5f8d9
matuszak47 <nikodem.matuszak@zsmeie.edu.torun.pl>
2024-11-29 15:31:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 function calculate(operation) {
-//Dpkończ kod - pobierz liczby i sprawdź czy są poprawne
+    const num1 = parseFloat(document.getElementById('number1').value);
+    const num2 = parseFloat(document.getElementById('number2').value);
+    let result = 0;
+    if (isNaN(num1) || isNaN(num2)) {
         alert('Proszę wprowadzić prawidłowe liczby.');
         return;
     }
-
     switch (operation) {
-       //Dokończ funkcję działań: dodawania, odejmowania, dzielenia, mnożenia
-       
-             //uzupełnij   alert('Nie można dzielić przez zero.');
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                alert('Nie można dzielić przez zero.');
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
             alert('Nieznana operacja.');
             return;
     }
-
     document.getElementById('result').textContent = result;
 }
